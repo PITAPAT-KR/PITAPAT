740526d4b2251de2a613f1a9a1b7dee1c9b5b671
HOOOO98 <loading2us@gmail.com>
2023-10-20 16:08:08 +0900

    folder setting

--- app/App.js
+++ app/App.js
@@ -4,15 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.navbar}>
-      <Text>Home</Text>
-      <Text>chat</Text>
-      <Text>community</Text>
-      <Text>Mypage</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  navbar: {},
-});
