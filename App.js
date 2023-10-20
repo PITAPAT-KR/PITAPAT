9c3be39520028e807630688dc264f55810fe0750
HOOOO98 <loading2us@gmail.com>
2023-10-20 14:44:25 +0900

    collaboration initial settings

--- App.js
+++ App.js
@@ -3,18 +3,16 @@ import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Pitapat start!</Text>
+    <View style={styles.navbar}>
+      <Text>Home</Text>
+      <Text>chat</Text>
+      <Text>community</Text>
+      <Text>Mypage</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
+  navbar: {},
 });
