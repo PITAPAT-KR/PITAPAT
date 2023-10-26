b3e5b0e7033450091bb9a88bfb7d3d15649199f2
Yamyam-code <rnffjt@gmail.com>
2023-10-26 14:18:22 +0900

    Feat: json데이터 기반으로 피드 생성

--- app/App.js
+++ app/App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { Feed } from './components/feed';
 
 export default function App() {
@@ -13,7 +13,8 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    width: '100%',
-    height: '100%',
+    flex: 1,
+    justifyContent: 'center',
+    paddingTop: '12%',
   },
 });
--- app/components/feed.js
+++ app/components/feed.js
@@ -1,53 +1,100 @@
 import { useEffect, useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
 
 const Feed = () => {
-  const [feedData, setFeedData] = useState(0);
+  const [feedData, setFeedData] = useState([]);
+  const [wholeData, setWholeData] = useState([]);
   const [name, setName] = useState('');
+  const [page, setPage] = useState(5);
   useEffect(() => {
     data = require('../json/feed.json');
-    setFeedData(data);
+    setWholeData(data.leaders);
+    setFeedData(data.leaders.slice(0, 5));
     setName(data.leaders[0].name);
   }, []);
-  return (
-    <View style={{ flex: 1 }}>
-      {feedData ? (
-        feedData.leaders.map((e, i) => (
-          <View key={i} style={styles.container}>
-            <View style={styles.imgBox}></View>
-            <View style={styles.textBox}>
-              <View style={styles.nameBox}>
-                <Text>{e.name}</Text>
-                <Text>{e.date}</Text>
-              </View>
-              <Text style={styles.infoText}>{e.info}</Text>
-            </View>
+  const renderItem = ({ item }) => {
+    return (
+      <View style={styles.container}>
+        <View style={styles.item}>
+          <View style={styles.imgBox}>
+            <View style={styles.img}></View>
           </View>
-        ))
-      ) : (
-        <View style={styles.container}>
-          <Text>하하하</Text>
+          <View style={styles.textBox}>
+            <View style={styles.nameBox}>
+              <Text style={styles.name}>{item.name}</Text>
+              <Text style={styles.date}>{item.date}</Text>
+            </View>
+            <Text style={styles.infoText}>{item.info}</Text>
+          </View>
         </View>
-      )}
-    </View>
+      </View>
+    );
+  };
+  const onEndReached = () => {
+    if (page + 5 < wholeData.length) {
+      setFeedData([...feedData, wholeData.slice(page, page + 5)]);
+      setPage(page + 5);
+    } else if (page + 5 > wholeData.length && page < wholeData.length) {
+      setFeedData([...feedData, wholeData.slice(page, wholeData.length - 1)]);
+    } else return;
+  };
+  return (
+    <FlatList
+      data={feedData}
+      onEndReached={onEndReached}
+      onEndReachedThreshold={0}
+      renderItem={renderItem}
+    />
   );
 };
 
 const styles = StyleSheet.create({
   container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+    height: 'auto',
+    paddingTop: '10%',
+  },
+  item: {
     flexDirection: 'row',
     justifyContent: 'space-between',
-    width: '100%',
-    height: '20%',
+    width: '95%',
+    padding: '5%',
+    paddingLeft: '-5%',
+    borderWidth: '1',
+    borderColor: 'black',
+    borderStyle: 'solid',
+    borderRadius: '10',
   },
   imgBox: {
     flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  img: {
+    width: 80,
+    height: 80,
     backgroundColor: 'gray',
+    borderRadius: '10',
   },
   textBox: {
     flex: 2,
+    justifyContent: 'space-around',
+    alignItems: 'flex-start',
   },
-  nameBox: {},
+  nameBox: {
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'flex-start',
+  },
+  name: {
+    fontWeight: '700',
+  },
+  date: {
+    fontSize: 10,
+  },
   infoText: {},
 });
 
